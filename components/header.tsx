cc3f9211662fd11d4dfa990d59a2b99b92b89ba8
Dany02S <77163542+Dany02S@users.noreply.github.com>
2026-02-20 22:02:41 +0200

    Init and main page

---
+++ components/header.tsx
@@ -0,0 +1,75 @@
+import {
+	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	NavigationMenuTrigger,
+	navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import Link from "next/link";
+import { ModeToggle } from "./ui/theme-switcher";
+import { link } from "fs/promises";
+
+const page_links = [
+	{
+		name: "Főoldal",
+		href: "/",
+	},
+	{
+		name: "Témák",
+		links: [
+			{
+				name: "Téma 1",
+				href: "/blogs/blog-1",
+			},
+			{
+				name: "Téma 2",
+				href: "/blogs/blog-2",
+			},
+		],
+	},
+];
+
+export default function Header() {
+	return (
+		<div className="flex h-15 fixed z-40 w-full items-center justify-center border-b bg-background px-4">
+			<NavigationMenu>
+				<NavigationMenuList>
+					{page_links.map((link) => {
+						if (link.links) {
+							return (
+								<NavigationMenuItem key={link.name}>
+									<NavigationMenuTrigger>{link.name}</NavigationMenuTrigger>
+									<NavigationMenuContent className="w-60">
+										{link.links.map((sublink) => (
+											<NavigationMenuLink
+												key={sublink.name}
+												asChild
+												className="w-52">
+												<Link href={sublink.href}>{sublink.name}</Link>
+											</NavigationMenuLink>
+										))}
+									</NavigationMenuContent>
+								</NavigationMenuItem>
+							);
+						} else {
+							return (
+								<NavigationMenuItem key={link.name}>
+									<NavigationMenuLink
+										asChild
+										className={navigationMenuTriggerStyle()}>
+										<Link href={link.href}>{link.name}</Link>
+									</NavigationMenuLink>
+								</NavigationMenuItem>
+							);
+						}
+					})}
+				</NavigationMenuList>
+			</NavigationMenu>
+			<div className="ml-auto">
+				<ModeToggle />
+			</div>
+		</div>
+	);
+}
